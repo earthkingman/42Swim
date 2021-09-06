b26a7f9e35a46d4f2e30c2580ba89af36f2fa142
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-09-06 23:46:01 +0900

    set: webpack react typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Counter } from './Counter'
+
+export const App = () => {
+  return (
+    <>
+      <h1>React TypeScripdfdft Webpack Starter Template</h1>
+      <div>hid</div>
+      <Counter />
+    </>
+  )
+}
