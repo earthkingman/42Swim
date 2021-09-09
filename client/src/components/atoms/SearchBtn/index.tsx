2f7254b95093bbbbfcf11167946441bc60eb67d9
Chloekkk <skdusdl8804@gmail.com>
2021-09-09 19:46:19 +0900

    add : atoms/searchbtn

---
+++ client/src/components/atoms/SearchBtn/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SearchBtn = () => {
+  return (
+    <svg
+      width="21"
+      height="20"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="8" cy="8" r="6.5" stroke="black" strokeWidth="3" />
+      <path d="M13.5 13L19 18.5" stroke="black" strokeWidth="3" />
+    </svg>
+  )
+}
+
+export default SearchBtn
