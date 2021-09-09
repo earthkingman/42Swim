653afe058c3a667662e04366ddd996a17a2ab77d
Chloekkk <skdusdl8804@gmail.com>
2021-09-09 19:54:50 +0900

    add : modal component

---
+++ client/src/components/atoms/ModalBox/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledModalBox = styled.div`
+  width: 533px;
+  height: 644px;
+  border-radius: 16px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 2px 4px 0px;
+`
