526c46cd8b98c4d219c868a378dcb777e3b031d7
Chloekkk <skdusdl8804@gmail.com>
2021-10-12 15:32:02 +0900

    :recycle: refactor : vite setting & client dir to frontend dir

--- client_2/src/components/pages/Detail/template/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Question = styled.div`
-  margin-bottom: 7rem;
-`;
-export const Answer = styled.div`
-  margin-bottom: 4rem;
-`;
--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -1,5 +1,2 @@
 declare module "*.png";
-declare module "*.svg" {
-	const content: string;
-	export default content;
-}
+declare module "*.svg";
