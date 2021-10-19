99c1c57620922d400e77c0a8dd3bb7817927fdda
Chloekkk <skdusdl8804@gmail.com>
2021-10-19 19:12:48 +0900

    :memo: : main-page-api state management

--- frontend/src/components/organisms/QuestionList/index.tsx
+++ frontend/src/components/organisms/QuestionList/index.tsx
@@ -11,59 +11,61 @@ export interface Props {
 }
 
 const QuestionList = ({ data, menu, setMenu, ...props }: Props) => {
-  return (
-    <S.QLWrapper>
-      <Tab {...props}>
-        <TabItem
-          active={menu === 0 ? true : false}
-          onTabClick={() => setMenu(0)}
-        >
-          최신순
-        </TabItem>
-        <TabItem
-          active={menu === 1 ? true : false}
-          onTabClick={() => setMenu(1)}
-        >
-          인기순
-        </TabItem>
-        <TabItem
-          active={menu === 2 ? true : false}
-          onTabClick={() => setMenu(2)}
-        >
-          답변 필요
-        </TabItem>
-        <TabItem
-          active={menu === 3 ? true : false}
-          onTabClick={() => setMenu(3)}
-        >
-          태그
-        </TabItem>
-      </Tab>
-      <S.List>
-        {data.map((d, idx) => (
-          <ListItem
-            title={d.title}
-            desc={d.desc}
-            check={d.check}
-            answer_cnt={d.answer_cnt}
-            like_cnt={d.like_cnt}
-            view_cnt={d.view_cnt}
-            tag={d.tag}
-            create_time={d.create_time}
-            key={idx}
-            {...props}
-          />
-        ))}
-        <ColumnSADiv height="115px">
-          <Pagination
-            onFront={() => console.log("front")}
-            onBack={() => console.log("back")}
-            page={1}
-          />
-        </ColumnSADiv>
-      </S.List>
-    </S.QLWrapper>
-  );
+  if (!data) return <div>Loading</div>;
+  else
+    return (
+      <S.QLWrapper>
+        <Tab {...props}>
+          <TabItem
+            active={menu === 0 ? true : false}
+            onTabClick={() => setMenu(0)}
+          >
+            최신순
+          </TabItem>
+          <TabItem
+            active={menu === 1 ? true : false}
+            onTabClick={() => setMenu(1)}
+          >
+            인기순
+          </TabItem>
+          <TabItem
+            active={menu === 2 ? true : false}
+            onTabClick={() => setMenu(2)}
+          >
+            답변 필요
+          </TabItem>
+          <TabItem
+            active={menu === 3 ? true : false}
+            onTabClick={() => setMenu(3)}
+          >
+            태그
+          </TabItem>
+        </Tab>
+        <S.List>
+          {data.map((d, idx) => (
+            <ListItem
+              title={d.title}
+              text={d.text}
+              is_solved={d.is_solved}
+              answer_cnt={d.answer_cnt}
+              like_count={d.like_count}
+              view_count={d.view_count}
+              hashtag={d.hashtag}
+              created_at={d.created_at}
+              key={idx}
+              {...props}
+            />
+          ))}
+          <ColumnSADiv height="115px">
+            <Pagination
+              onFront={() => console.log("front")}
+              onBack={() => console.log("back")}
+              page={1}
+            />
+          </ColumnSADiv>
+        </S.List>
+      </S.QLWrapper>
+    );
 };
 
 export default QuestionList;
