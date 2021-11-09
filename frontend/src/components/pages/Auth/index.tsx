8f3c206152a0df36233b23435d0ba219ef603ce5
Chloekkk <skdusdl8804@gmail.com>
2021-11-09 13:57:01 +0900

    :sparkles: add route : add redirect auth route

---
+++ frontend/src/components/pages/Auth/index.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Auth = ({ ...props }) => {
+  const query = location.search;
+
+  console.log("query", query);
+
+  useEffect(() => {
+    const data = axios.get(`http://localhost/auth/authResult${query}`);
+    console.log("data", data);
+  }, [query]);
+
+  //   console.log("match", props.match);
+  //   console.log("location", props.location);
+  //   console.log(location.search);
+  return <div>Hi</div>;
+};
+
+export default Auth;
