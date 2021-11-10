65d20834cc9e150bfcb839fae3dd099503c5d372
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-11-10 18:09:47 +0900

    ♻️ fix: useSWR -> useSWRImmutable

--- frontend/src/components/molecules/ListItem/index.tsx
+++ frontend/src/components/molecules/ListItem/index.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import dateChange from "../../../utils/dateChange";
 import { RowSBDiv } from "../../atoms/Div";
 import Tag from "../../atoms/Tag";
-import Profile from "../Profile";
 import SortCounter from "../SortCounter";
 import * as S from "./style";
 
@@ -10,7 +9,7 @@ export interface Props {
   id: number;
   title: string;
   text: string;
-  nickname: string;
+  nickname?: string;
   is_solved: boolean;
   answer_cnt: number;
   like_count: number;
@@ -24,7 +23,6 @@ const ListItem = ({
   title,
   text,
   is_solved,
-  nickname,
   answer_cnt = 1,
   like_count,
   view_count,
@@ -41,17 +39,14 @@ const ListItem = ({
         <S.Desc size="18" weight="normal" color="grey">
           {text}
         </S.Desc>
-        <S.Bottom>
-          <RowSBDiv>
-            <S.Time size="14" weight="normal" color="grey">
-              {dateChange(created_at)}
-            </S.Time>
-            {hashtag.map((data, idx) => (
-              <Tag name={data?.name} key={idx} />
-            ))}
-          </RowSBDiv>
-          <Profile size="sm" nickname={nickname} id={0} />
-        </S.Bottom>
+        <RowSBDiv>
+          <S.Time size="14" weight="normal" color="grey">
+            {dateChange(created_at)}
+          </S.Time>
+          {hashtag.map((data, idx) => (
+            <Tag name={data?.name} key={idx} />
+          ))}
+        </RowSBDiv>
       </S.Content>
       {/* <S.Check>{check ? <CheckImg /> : <></>}</S.Check> */}
       <S.Counter>
