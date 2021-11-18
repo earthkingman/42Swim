fff1595f080d938ff55d968e246e63044b92fd1e
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-11-18 18:50:12 +0900

    Answer Props 수정

--- frontend/src/components/molecules/Answer/index.tsx
+++ frontend/src/components/molecules/Answer/index.tsx
@@ -6,16 +6,16 @@ import Profile, { ProfileProps } from "../Profile";
 import { AnswerTitleWrapper, AnswerWrapper } from "./sytle";
 
 export interface AnswerProps {
-  id?: number;
+  key?: number;
   user: ProfileProps;
   created_at: string;
   text: string;
 }
 
-const Answer = ({ created_at, user, text }: AnswerProps) => {
+const Answer = ({ created_at, user, text, ...props }: AnswerProps) => {
   const createAt = dateChange(created_at);
   return (
-    <AnswerWrapper>
+    <AnswerWrapper {...props}>
       <AnswerTitleWrapper>
         <Profile {...user} size="sm" />
         <Text size="14" color="grey">
