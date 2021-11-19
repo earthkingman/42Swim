6069aa587c3a355e420cfb6b4c138bc23afd2088
Chloekkk <skdusdl8804@gmail.com>
2021-11-20 01:29:31 +0900

    :recycle: 수정 : question map error 수정

--- frontend/src/components/organisms/QuestionList/index.tsx
+++ frontend/src/components/organisms/QuestionList/index.tsx
@@ -57,7 +57,8 @@ const QuestionList = ({ menu, setMenu, ...props }: Props) => {
                   <Skeleton />
                 </S.SkeletonItem>
               ))
-            : question?.quesiontList.map((d, idx) => (
+            : question?.quesiontList &&
+              question?.quesiontList.map((d, idx) => (
                 <ListItem
                   id={d.id}
                   title={d.title}
