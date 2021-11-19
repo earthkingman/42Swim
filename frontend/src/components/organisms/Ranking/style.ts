84a5c124653101b0ed114222551cc70f8102b485
Chloekkk <skdusdl8804@gmail.com>
2021-11-19 10:46:32 +0900

    :sparkles: 업데이트 : 42login 으로 변경

---
+++ frontend/src/components/organisms/Ranking/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const RankingDiv = styled.div`
+  width: 27%;
+
+  @media (max-width: 910px) {
+    display: none;
+    background-color: pink;
+  }
+`;
+
+export const Winner = styled.div`
+  width: 100%;
+`;
--- frontend/src/components/pages/Main/Main.tsx
+++ frontend/src/components/pages/Main/Main.tsx
@@ -2,33 +2,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import PlusIcon from "../../asset/icons/PlusIcon";
-import Box from "../../atoms/Box";
 import Button from "../../atoms/Button";
-import { RowSADiv } from "../../atoms/Div";
-import Title from "../../atoms/Title";
-import Text from "../../atoms/Text";
 import SearchInput from "../../molecules/SearchInput";
 import Header from "../../organisms/Header";
 import QuestionList from "../../organisms/QuestionList";
 import MainTemplate from "./template";
-import Crown from "../../asset/icons/Crown";
 import BasicTemplate from "../BasicTemplate";
-import CatWork from "../../asset/png/Cats-at-Work.png";
 import useInput from "../../../hooks/useInput";
-import styled from "styled-components";
+import Ranking from "../../organisms/Ranking";
 
 interface Props {
   history: any;
 }
-
-const RankingDiv = styled.div`
-  width: 27%;
-
-  @media (max-width: 910px) {
-    display: none;
-    background-color: pink;
-  }
-`;
 
 const MainPage = ({ history, ...props }: Props) => {
   const [menu, setMenu] = useState(0);
@@ -42,7 +27,7 @@ const MainPage = ({ history, ...props }: Props) => {
 
   return (
     <>
-      <BasicTemplate header={<Header />}>
+      <BasicTemplate>
         <MainTemplate
           panel={
             <>
@@ -61,46 +46,7 @@ const MainPage = ({ history, ...props }: Props) => {
             </>
           }
           content1={<QuestionList menu={menu} setMenu={setMenu} />}
-          content2={
-            <RankingDiv>
-              <RowSADiv
-                style={{
-                  height: "50px",
-                  justifyContent: "flex-start",
-                  alignItems: "flex-start",
-                }}
-              ></RowSADiv>
-              <Box
-                width="100%"
-                height="338px"
-                style={{
-                  display: "flex",
-                  padding: "4rem",
-                  justifyContent: "flex-start",
-                  alignItem: "center",
-                  flexDirection: "column",
-                  position: "relative",
-                }}
-              >
-                <Crown />
-                <Title size="h2" style={{ lineHeight: "1.3" }}>
-                  King of 42
-                  <br /> 준비중...
-                </Title>
-                <img
-                  src={CatWork}
-                  alt="img"
-                  style={{
-                    position: "absolute",
-                    bottom: "0px",
-                    left: "-0px",
-                    width: "10rem",
-                    borderRadius: "16px",
-                  }}
-                />
-              </Box>
-            </RankingDiv>
-          }
+          content2={<Ranking />}
         />
       </BasicTemplate>
     </>
