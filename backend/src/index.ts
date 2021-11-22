f02a1153775f9380d8d77e94accfa88da8d62334
Inho Ha <inhoha@Inhos-MacBook-Pro.local>
2021-11-23 02:51:16 +0900

    sentry 적용

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,15 +17,25 @@ import path from 'path';
 import { applicationRouter } from "./routes";
 
 import { insertSeed } from "./entity/seed/seed_data"
+import * as Sentry from "@sentry/node";
 
 dotenv.config();
+
 const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'))
 const app = express();
+if (process.env.NODE_ENV === 'production') {
+	Sentry.init({dsn: process.env.SENTRY_DSN});
+	app.use(Sentry.Handlers.requestHandler());
+}
+
 passportConfig();
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.get("/debug-sentry", function mainHandler(req, res) {
+	throw new Error("My first Sentry error!");
+  });
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 const corsOptions = {
@@ -34,8 +44,10 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(applicationRouter);
+if (process.env.NODE_ENV === 'production') {
+	app.use(Sentry.Handlers.errorHandler());
+}
 app.use(errorMiddleware);
-
 
 app.listen(5000, async () => {
 	console.log("서버 가동");
