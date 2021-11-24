f7fe4dbbd2e873a1df3518e4c2f3d45d833c37a8
Chloekkk <skdusdl8804@gmail.com>
2021-11-24 23:59:19 +0900

    :recycle: fix: fix 42-login Auth

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -2,20 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export = {
-	type: "mysql",
-	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
-	username: process.env.MYSQL_USER,
-	password: process.env.MYSQL_ROOT_PASSWORD,
-	database: process.env.MYSQL_DATABASE,
-	synchronize: false,
-	logging: false,
-	entities: ["src/entity/**/*.ts"],
-	migrations: ["src/migration/**/*.ts"],
-	subscribers: ["src/subscriber/**/*.ts"],
-	cli: {
-		entitiesDir: "src/entity",
-		migrationsDir: "src/migration",
-		subscribersDir: "src/subscriber",
-	},
+  type: "mysql",
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: false,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,27 +5,27 @@ import { authRouter } from "./routes/auth";
 import { postRouter } from "./routes/post";
 import { userRouter } from "./routes/user";
 import { pageRouter } from "./routes/page";
-import { errorMiddleware } from "./middlewares/error.middleware"
+import { errorMiddleware } from "./middlewares/error.middleware";
 import cors from "cors";
 import passport from "passport";
 import passportConfig from "./passport";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 import { applicationRouter } from "./routes";
 
-import { insertSeed } from "./entity/seed/seed_data"
+import { insertSeed } from "./entity/seed/seed_data";
 import * as Sentry from "@sentry/node";
 
 dotenv.config();
 
-const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'))
+const swaggerSpec = YAML.load(path.join(__dirname, "../build/swagger.yaml"));
 const app = express();
-if (process.env.NODE_ENV === 'production') {
-	Sentry.init({dsn: process.env.SENTRY_DSN});
-	app.use(Sentry.Handlers.requestHandler());
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
+  app.use(Sentry.Handlers.requestHandler());
 }
 
 passportConfig();
@@ -33,22 +33,28 @@ app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const corsOptions = {
-	origin: ["http://localhost:3000", "http://3.36.121.236", "http://42sof.justdev.net","https://42sof.justdev.net","https://3.36.121.236"],
-	credentials: true,
+  origin: [
+    "http://localhost:3000",
+    "http://3.36.121.236",
+    "http://42sof.justdev.net",
+    "https://42sof.justdev.net",
+    "https://3.36.121.236",
+  ],
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(applicationRouter);
-if (process.env.NODE_ENV === 'production') {
-	app.use(Sentry.Handlers.errorHandler());
+if (process.env.NODE_ENV === "production") {
+  app.use(Sentry.Handlers.errorHandler());
 }
 app.use(errorMiddleware);
 
 app.listen(5000, async () => {
-	console.log("서버 가동");
-	await createConnection();
-	console.log("DB 연결");
-	// await insertSeed();
+  console.log("서버 가동");
+  await createConnection();
+  console.log("DB 연결");
+  //   await insertSeed();
 });
