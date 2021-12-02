5e1d0acc900a741e2d4825ff623cce76f76d43b5
hainho <hainho9704@gmail.com>
2021-12-02 20:12:37 +0900

    질문글 상세보기 API 비로그인유저 처리 로직 추가

--- backend/src/service/page_service.ts
+++ backend/src/service/page_service.ts
@@ -87,6 +87,82 @@ export class PageService {
 			view_count: questionInfo.view_count,
 			title: questionInfo.title,
 			text: questionInfo.text,
+			is_like: questionInfo.question_like[0].is_like,
+		};
+		if (questionInfo.answer) {
+			for (let i = 0; i < questionInfo.answer.length; i++) {
+				const curAnswer = questionInfo.answer[i];
+				const AnswerDetail: AnswerDetail = {
+					id: curAnswer.id,
+					created_at: curAnswer.created_at,
+					updated_at: curAnswer.updated_at,
+					question: curAnswer.question,
+					user: curAnswer.user,
+					comment: curAnswer.comment,
+					answer_like: undefined,
+					like_count: curAnswer.like_count,
+					text: curAnswer.text,
+					is_chosen: curAnswer.is_chosen,
+					is_like: questionInfo.answer[i].answer_like[0].is_like,
+				}
+				questionDetailInfo.answer.push(AnswerDetail);
+			}
+		}
+		return questionDetailInfo;
+	}
+
+	async getQuestionDetailNoAuth(questionId) {
+
+		const subQuery = await this.questionRepository
+			.createQueryBuilder('covers')
+			.select(['covers.id'])
+			.where('covers.id = :questionId', { questionId })
+
+		const questionInfo = await this.questionRepository
+			.createQueryBuilder('question')
+			.innerJoin(`(${subQuery.getQuery()})`, 'covers',
+				'question.id = covers.covers_id')
+			.setParameters(subQuery.getParameters())
+			.leftJoin('question.hashtag', 'hashtag')
+			.leftJoin('question.user', 'question_user')
+			.leftJoin('question.comment', 'question_comment')
+			.leftJoin('question.answer', 'answer')
+			.leftJoin('answer.comment', 'answer_comment')
+			.leftJoin('answer.user', 'answer_user')
+			.leftJoin('question_comment.user', 'question_comment_user')
+			.leftJoin('answer_comment.user', 'answer_comment_user')
+			.leftJoin('question.question_like', 'question_like')
+			.leftJoin('question_like.user', 'question_like_user')
+			.leftJoin('answer.answer_like', 'answer_like')
+			.leftJoin('answer_like.user', 'answer_like_user')
+			.select(['question.id', 'question.created_at', 'question.is_solved', 'question.like_count', 'question.view_count', 'question.title', 'question.text',
+				'answer.id', 'answer.created_at', 'answer.like_count', 'answer.text', 'answer.is_chosen',
+				'question_comment.id', 'question_comment.created_at', 'question_comment.text',
+				'answer_comment.id', 'answer_comment.created_at', 'answer_comment.text',
+				'question_user.id', 'question_user.created_at', 'question_user.email', 'question_user.nickname', 'question_user.photo',
+				'question_comment_user.id', 'question_comment_user.created_at', 'question_comment_user.email', 'question_comment_user.nickname', 'question_comment_user.photo',
+				'answer_user.id', 'answer_user.created_at', 'answer_user.email', 'answer_user.nickname', 'answer_user.photo',
+				'answer_comment_user.id', 'answer_comment_user.created_at', 'answer_comment_user.email', 'answer_comment_user.nickname', 'answer_comment_user.photo',
+				'hashtag.id', 'hashtag.name',
+			])
+			.disableEscaping()
+			.getOne();
+
+		const questionDetailInfo: QuestionDetail = {
+			id: questionInfo.id,
+			created_at: questionInfo.created_at,
+			updated_at: questionInfo.updated_at,
+			user: questionInfo.user,
+			answer: [],
+			comment: questionInfo.comment,
+			hashtag: questionInfo.hashtag,
+			question_like: undefined,
+			is_solved: questionInfo.is_solved,
+			answer_count: questionInfo.answer_count,
+			like_count: questionInfo.like_count,
+			view_count: questionInfo.view_count,
+			title: questionInfo.title,
+			text: questionInfo.text,
 			is_like: null,
 		};
 		if (questionInfo.answer) {
@@ -106,21 +182,6 @@ export class PageService {
 					is_like: null,
 				}
 				questionDetailInfo.answer.push(AnswerDetail);
-			}
-		}
-
-		if (userId) {
-			const questionLike = questionInfo.question_like[0].is_like;
-			if (questionLike) {
-				questionDetailInfo.is_like = questionLike;
-			}
-			if (questionInfo.answer) {
-				for (let i = 0; i < questionInfo.answer.length; i++) {
-					const answerLike = questionInfo.answer[i].answer_like[0].is_like;
-					if (answerLike) {
-						questionDetailInfo.answer[i].is_like = answerLike;
-					}
-				}
 			}
 		}
 		return questionDetailInfo;
