1002dac16a7470c550ba79cbbc29543cf7c8fea9
hainho <hainho9704@gmail.com>
2021-12-13 22:23:50 +0900

    :sparkles: Feat : 해시태그 아이디로 질문글 리스트 페이지 얻기 API 작성

--- backend/src/service/page_service.ts
+++ backend/src/service/page_service.ts
@@ -5,7 +5,6 @@ import { HashTag } from "../entity/hashtag";
 import { Question } from "../entity/question";
 import { QuestionLike } from "../entity/question_like";
 import { AnswerLike } from "../entity/answer_like";
-import { length } from "class-validator";
 import { QuestionDetail } from "../definition/response_data";
 import { AnswerDetail } from "../definition/response_data";
 
@@ -13,11 +12,13 @@ export class PageService {
 	private questionRepository: Repository<Question>;
 	private questionLikeRepository: Repository<QuestionLike>;
 	private answerLikeRepository: Repository<AnswerLike>;
+	private hashtagRepository: Repository<HashTag>;
 
 	constructor() {
 		this.questionRepository = getConnection().getRepository(Question);
 		this.questionLikeRepository = getConnection().getRepository(QuestionLike);
 		this.answerLikeRepository = getConnection().getRepository(AnswerLike);
+		this.hashtagRepository = getConnection().getRepository(HashTag);
 	}
 
 	async setQuestionViewCount(questionId) {
@@ -337,4 +338,37 @@ export class PageService {
 			.getCount();
 		return { questionList, questionCount }
 	}
+
+	async getQuestionListByHashtagId(pageInfo): Promise<any> {
+        const subQuery = await this.questionRepository
+            .createQueryBuilder('covers')
+            .select(['covers.id'])
+            .leftJoin('covers.hashtag', 'hashtag')
+            .where('hashtag.id = :id', { id: pageInfo.hashtagId })
+            .orderBy('covers.id', 'DESC')
+            .limit(pageInfo.limit)
+            .offset(pageInfo.offset)
+
+        const questionList = await this.questionRepository
+            .createQueryBuilder('question')
+            .innerJoin(`(${subQuery.getQuery()})`, 'covers',
+                'question.id = covers.covers_id')
+            .setParameters(subQuery.getParameters())
+            .innerJoinAndSelect('question.user', 'question_user')
+            .leftJoin('question.hashtag', 'question_hashtag')
+            .select(['question.id', 'question.created_at', 'question.is_solved', 'question.like_count', 'question.view_count', 'question.answer_count', 'question.title', 'question.text',
+                'question_user.id', 'question_user.created_at', 'question_user.email', 'question_user.nickname', 'question_user.photo',
+                'question_hashtag.id', 'question_hashtag.name'
+            ])
+            .getMany();
+
+        const questionCount = await await this.hashtagRepository
+            .createQueryBuilder('hashtag')
+            .leftJoinAndSelect('hashtag.question', 'question')
+            .where('hashtag.name = :name', { name: pageInfo.hashtag })
+            .select('COUNT(*) AS count')
+            .getRawOne();
+
+        return { questionList, questionCount: questionCount.count };
+    }
 }
