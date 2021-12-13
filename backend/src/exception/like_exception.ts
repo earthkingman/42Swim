7ee941adb5137fe00f5b16debb051ff400338e42
ji-park <qkrskdjf@naver.com>
2021-12-14 03:06:32 +0900

    :hammer: 싫어요 예외처리

--- backend/src/exception/like_exception.ts
+++ backend/src/exception/like_exception.ts
@@ -1,19 +1,25 @@
 import { HttpException } from "./http_exception";
 
 export class LikeBadRequestException extends HttpException {
-    constructor(msg : string) {
+    constructor(msg: string) {
         super(400, `해당 좋아요에 대한 잘못된 요청입니다. ${msg}`);
     }
 }
 
 export class LikeForbiddenException extends HttpException {
-    constructor(msg : string) {
+    constructor(msg: string) {
         super(403, `해당 좋아요에 대한 권한이 없습니다. ${msg}`);
     }
 }
 
 export class LikeNotFoundException extends HttpException {
-    constructor(msg : string) {
+    constructor(msg: string) {
         super(404, `해당 좋아요를 찾을 수 없습니다. ${msg}`);
+    }
+}
+
+export class HateException extends HttpException {
+    constructor() {
+        super(400, `유저의 점수가 0점이라 싫어요가 불가능합니다.`);
     }
 }
