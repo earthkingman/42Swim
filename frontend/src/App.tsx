d2db95121a9d29a2f77ad83b884376a2ad1530a5
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-12-17 21:35:48 +0900

    ✨ feat: 구글 애널리틱스 페이지네이션 연동 완료 #1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
 import { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Auth from "./components/pages/Auth";
 import DetailPage from "./components/pages/Detail/Detail";
 import EditPage from "./components/pages/Edit/Edit";
@@ -25,57 +20,49 @@ ReactGA.exception({
 });
 
 const App: React.FC = () => {
-  const history = useHistory();
+  const location = useLocation();
   useEffect(() => {
-    if (import.meta.env.MODE === "development") {
-      ReactGA.initialize(import.meta.env.VITE_GA_ID, { debug: true });
-    } else {
+    if (import.meta.env.MODE !== "development") {
       ReactGA.initialize(import.meta.env.VITE_GA_ID);
-    }
-    history.listen((location) => {
-      console.log("location :>> ", location);
       ReactGA.set({ page: location.pathname }); // Update the user's current page
       ReactGA.pageview(location.pathname); // Record a pageview for the given page
-    });
-  }, [history]);
-  console.log("location :>> ", location);
+    }
+  }, [location]);
   return (
     <GlobalThemeProvider>
-      <Router>
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={(props: any) => <MainPage {...props} />}
-          />
-          <Route
-            path="/search"
-            exact
-            render={(props: any) => <SearchPage {...props} />}
-          />
-          <Route
-            path="/detail"
-            exact
-            render={(props: any) => <DetailPage {...props} />}
-          />
-          <Route
-            path="/writing"
-            exact
-            render={(props: any) => <WritingPage {...props} />}
-          />
-          <Route
-            path="/edit"
-            exact
-            render={(props: any) => <EditPage {...props} />}
-          />
-          <Route
-            path="/setting"
-            exact
-            render={(props: any) => <SettingPage {...props} />}
-          />
-          <Route path="/auth" render={(props: any) => <Auth {...props} />} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={(props: any) => <MainPage {...props} />}
+        />
+        <Route
+          path="/search"
+          exact
+          render={(props: any) => <SearchPage {...props} />}
+        />
+        <Route
+          path="/detail"
+          exact
+          render={(props: any) => <DetailPage {...props} />}
+        />
+        <Route
+          path="/writing"
+          exact
+          render={(props: any) => <WritingPage {...props} />}
+        />
+        <Route
+          path="/edit"
+          exact
+          render={(props: any) => <EditPage {...props} />}
+        />
+        <Route
+          path="/setting"
+          exact
+          render={(props: any) => <SettingPage {...props} />}
+        />
+        <Route path="/auth" render={(props: any) => <Auth {...props} />} />
+      </Switch>
     </GlobalThemeProvider>
   );
 };
