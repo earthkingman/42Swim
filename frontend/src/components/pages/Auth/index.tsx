1b547f6fd643c9b938bc6581770a7f74556853a1
Chloekkk <skdusdl8804@gmail.com>
2021-12-20 22:58:54 +0900

    :recycle: fix: login 새로고침 이슈 수정 & main 최신화

--- frontend/src/components/pages/Auth/index.tsx
+++ frontend/src/components/pages/Auth/index.tsx
@@ -1,30 +1,27 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Redirect } from "react-router";
-import Loading from "../../atoms/Loading";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Auth = ({ ...props }) => {
   const query = location.search;
-  const [loading, setLoading] = useState(true);
 
   console.log("query", query);
 
   useEffect(() => {
-    const data = axios.get(
-      `${import.meta.env.VITE_API_HOST}/auth/authResult${query}`
-    );
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-    console.log("data", data);
+    const getInfo = async () => {
+      await axios.get(
+        `${import.meta.env.VITE_API_HOST}/auth/authResult${query}`
+      );
+    };
+    getInfo();
+    // }, 5000);
   }, [query]);
 
   //   console.log("match", props.match);
   //   console.log("location", props.location);
   //   console.log(location.search);
-  if (loading) return <Loading visible={loading} />;
-  else return <Redirect to="/" />;
+  return <Redirect to="/" />;
 };
 
 export default Auth;
