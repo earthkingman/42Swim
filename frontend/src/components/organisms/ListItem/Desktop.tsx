ea24a913c4d78b2edc6bd04535f0ad4a538529d5
Chloekkk <skdusdl8804@gmail.com>
2021-12-30 23:37:01 +0900

    :sparkles: update : main 페이지 리스트 ListItem 모바일 버전 적용

---
+++ frontend/src/components/organisms/ListItem/Desktop.tsx
@@ -0,0 +1,75 @@
+import { Props } from ".";
+import axios from "axios";
+import * as S from "./style";
+import { Link } from "react-router-dom";
+import dateChange from "../../../utils/dateChange";
+import { RowSBDiv } from "../../atoms/Div";
+import Tag from "../../atoms/Tag";
+import Profile from "../../molecules/Profile";
+import SortCounter from "../../molecules/SortCounter";
+
+const Desktop = ({
+  id,
+  title,
+  text,
+  is_solved,
+  photo,
+  nickname,
+  answer_cnt = 1,
+  like_count,
+  view_count,
+  created_at,
+  hashtag,
+  ...props
+}: Props) => {
+  text = text
+    .replace(/^\s*`{3}\w*/gm, "")
+    .replace(/[`#*~_>]/g, "")
+    .replace(/^!\[[\w.]*\]\([\w.:/-]+\)/gi, "");
+  if (hashtag.length > 5) {
+    hashtag = hashtag.slice(0, 3);
+    hashtag.push({
+      name: "...",
+    });
+  }
+
+  const onClick = () => {
+    const apiUrl = `${import.meta.env.VITE_API_HOST}/pages/question/viewCount`;
+    axios.post(apiUrl, { questionId: id }, { withCredentials: true });
+  };
+
+  return (
+    <S.ListBox {...props}>
+      <S.Content size="lg">
+        <Link to={`/detail?id=${id}`} onClick={onClick}>
+          <S.Title size="h2">{title}</S.Title>
+        </Link>
+        <S.Desc size="18" weight="normal" color="grey">
+          {text}
+        </S.Desc>
+        <S.Bottom>
+          <RowSBDiv>
+            <S.Time size="14" weight="normal" color="grey">
+              {dateChange(created_at)}
+            </S.Time>
+            {hashtag.map((data, idx) => (
+              <Tag name={data?.name} key={idx} style={{ marginTop: "0px" }} />
+            ))}
+          </RowSBDiv>
+          <Profile size="sm" photo={photo} nickname={nickname} id={0} />
+        </S.Bottom>
+      </S.Content>
+      <S.Counter>
+        <SortCounter
+          name="답변"
+          count={answer_cnt}
+          color={is_solved ? "primary" : "black"}
+        />
+        <SortCounter name="추천" count={like_count} />
+        <SortCounter name="조회" count={view_count} />
+      </S.Counter>
+    </S.ListBox>
+  );
+};
+
+export default Desktop;
