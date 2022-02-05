0e469cabf599da9a7d0942eb676252b808bf4c3b
ji-park <qkrskdjf@naver.com>
2022-02-06 01:12:17 +0900

    BugFix : 닉네임 글자수 제한

--- backend/src/controllers/dto/user.ts
+++ backend/src/controllers/dto/user.ts
@@ -1,10 +1,7 @@
 import { Min, Max, Length, IsEmail, IsString } from "class-validator";
 
 export class User {
-    // @IsEmail()
-    // private readonly email: string;
 
-    // @IsString()
-    // @Length(1, 10)
-    // private readonly nickname: string;
+    @Length(1, 15)
+    private readonly nickname: string;
 }
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,6 +3,8 @@ import { authJwt, userInfoAuth } from "../middlewares/auth.middleware";
 import { UserController } from "../controllers/user"
 import { RankController } from "../controllers/rank";
 import { s3Util } from "../aws/s3_utils";
+import { validationMiddleware } from "../middlewares/validation.middleware"
+import { User } from "../controllers/dto/user"
 
 export const userRouter = express.Router();
 
@@ -10,7 +12,7 @@ userRouter.get('/info', userInfoAuth, UserController.userInfo);
 userRouter.get('/profile', UserController.getProfile);
 userRouter.patch('/image', authJwt, s3Util.s3ImageUpload({ folder: 'author' }).single("imgFile"), UserController.updateUserImage);
 userRouter.delete('/image', authJwt, UserController.deleteUserImage);
-userRouter.patch('/nickname', authJwt, UserController.updateUserNickname)
+userRouter.patch('/nickname', authJwt, validationMiddleware(User), UserController.updateUserNickname)
 userRouter.patch('/password', authJwt, UserController.updateUserPassword)
 userRouter.patch('/email', authJwt, UserController.updateUserEmail)
 userRouter.get('/ranking', RankController.getRanking);
